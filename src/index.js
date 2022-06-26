c5d24c1542704501174ffbf5a689e9e9194baace
tatiana <name>
2022-06-26 17:25:34 +0700

    fix l

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = String(n);
+    if (n < 0) {
+        let stt = str.substr(1);
+        return Number(stt.split("").reverse().join(""));
+    } else
+        return Number(str.split("").reverse().join(""));
+};
